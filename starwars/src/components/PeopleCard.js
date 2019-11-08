9376c4277fc911d477f787998bc738fa9b3253f2
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2019-11-08 10:54:34 -0800

    added styled components

--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -4,22 +4,31 @@ import styled from "styled-components";
 export default function PeopleCard(props) {
 
     const CharacterCard = styled.div`
-        display: flex;
-        flex-direction: column;
-        border: 2px solid black;
+        border: 2px solid white;
         margin: 2%;
+        width: 400px;
+        height: 300px;
+        padding: 1%;
+        margin: 30px;
+        color: white;
+        transition: transform 0.3s linear;
 
+        &:hover{
+            color: yellow;
+            border: 2px solid yellow;
+            transform: translate(-5px) scale(1.05);
+        }
     `
 
     return (
         <CharacterCard>
             <h2>Name: {props.name}</h2>
-            <p>Birth Year: {props.birth_year}</p>
-            <p>Gender: {props.gender}</p>
-            <p>Hair Color: {props.hair_color}</p>
-            <p>Eye Color: {props.eye_color}</p>
-            <p>Height: {props.height}</p>
-            <p>Mass: {props.mass}</p> 
+                <p>Birth Year: {props.birth_year}</p>
+                <p>Gender: {props.gender}</p>
+                <p>Hair Color: {props.hair_color}</p>
+                <p>Eye Color: {props.eye_color}</p>
+                <p>Height: {props.height}</p>
+                <p>Mass: {props.mass}</p> 
         </CharacterCard>
     );
 }
